7673606550ae67a8f605724710311ca95da8345b
Nathan R Valentine <nrvale0@gmail.com>
2016-02-10 14:40:49 -0800

    don"t forget the whitespace

--- nextdoor/metadata.rb
+++ nextdoor/metadata.rb
@@ -1,10 +1,10 @@
 name        "nextdoor"
 maintainer  "Nextdoor.com"
 license     "see LICENSE file in repository root"
-description "Base scripts for Nextdoor.com on Linux (RLL) to initialize basic functionality"
+description "Nextdoor.com RL10 (only!) RightScripts",
 version     '0.1.0'
 
-recipe      "nextdoor::dump-env", "Print environment variables for debugging purposes"
+recipe      "nextdoor::dump-env", "Print environment variables for debugging"
 recipe      "nextdoor::mounts", "Configure /mnt"
 recipe      "nextdoor::hostname", "Configure hostname + domain"
 recipe      "nextdoor::puppet-install", "Install the Puppet agent"
@@ -17,7 +17,8 @@ attribute "AWS_ACCESS_KEY_ID",
           :description => ";)",
           :required => "required",
           :type => "string",
-          :recipes => ['nextdoor::mounts', 'nextdoor::elb-connect', 'nextdoor::elb-disconnect']
+          :recipes => ['nextdoor::mounts', 'nextdoor::elb-connect',
+                       'nextdoor::elb-disconnect']
 
 attribute "AWS_SECRET_ACCESS_KEY",
           :category => "CLOUD",
@@ -25,7 +26,8 @@ attribute "AWS_SECRET_ACCESS_KEY",
           :description => ";)",
           :required => "required",
           :type => "string",
-          :recipes => ['nextdoor::mounts', 'nextdoor::elb-connect', 'nextdoor::elb-disconnect']
+          :recipes => ['nextdoor::mounts', 'nextdoor::elb-connect',
+                       'nextdoor::elb-disconnect']
           
 attribute "DEBUG",
           :category => "NEXTDOOR",
@@ -33,12 +35,14 @@ attribute "DEBUG",
           :description => "** WARNING - debug mode may expose secrets!!! **",
           :require => "optional",
           :type => "string",
-          :recipes => ['nextdoor::dump-env', 'nextdoor::mounts', 'nextdoor::hostname', 'nextdoor::puppet-install']
+          :recipes => ['nextdoor::dump-env', 'nextdoor::mounts',
+                       'nextdoor::hostname', 'nextdoor::puppet-install']
 
 attribute "DEFAULT_DOMAIN",
           :category => "NEXTDOOR: HOSTNAME SETTINGS",
           :display_name => "DEFAULT_DOMAIN",
-          :description => "Domain name to attach to the hostname (ie, mycompany.com)",
+          :description => "Domain name to attach to the hostname"\
+                          " (ie, mycompany.com)",
           :require => "optional",
           :default => "cloud.nextdoor.com",
           :type => "string",
@@ -47,7 +51,10 @@ attribute "DEFAULT_DOMAIN",
 attribute "EBS_TYPE",
           :category => "STORAGE",
           :display_name => "EBS_TYPE",
-          :description => "gp2 for General Purpose (SSD) volumes, io1 for Provisioned IOPS (SSD) volumes, or standard for Magnetic volumes. If you choose io1, you'll get 4000 IOPS.",
+          :description => "gp2 for General Purpose (SSD) volumes, io1 for"\
+                          " Provisioned IOPS (SSD) volumes, or standard for"\
+                          " Magnetic volumes. If you choose io1, you'll get"\
+                          " 4000 IOPS.",
           :required => "optional",
           :type => "string",
           :default => "standard",
@@ -73,7 +80,9 @@ attribute "EC2_INSTANCE_ID",
 attribute "ELB_NAME",
           :category => "LOAD_BALANCER",
           :display_name => "ELB_NAME",
-          :description => "The is the name of the ELB you gave in RightScale or the Amazon AWS console. Note: This is the short name and *not* the FQDN.",
+          :description => "The is the name of the ELB you gave in RightScale"\
+                          " or the Amazon AWS console. Note: This is the short"\
+                          " name and *not* the FQDN.",
           :required => "optional",
           :type => "string",
           :recipes => ['nextdoor::elb-connect', 'nextdoor::elb-disconnect']
@@ -84,11 +93,13 @@ attribute "INSTANCE_ID",
           :description => "Cloud specific Instance ID (ie, i-12341234)",
           :require => "optional",
           :type => "string",
-          :recipes => ['nextdoor::dump-env', 'nextdoor::hostname', 'nextdoor::elb-connect', 'nextdoor::elb-disconnect']
+          :recipes => ['nextdoor::dump-env', 'nextdoor::hostname',
+                       'nextdoor::elb-connect', 'nextdoor::elb-disconnect']
 
 attribute "PUPPET_AGENT_VERSION",
           :display_name => "PUPPET_AGENT_VERSION",
-          :description  => "The Puppet agent version to install. ex: 'PC1', '3.8.1-1puppetlabs1'",
+          :description  => "Puppet agent version to install."\
+                           "ex: 'PC1', '3.8.1-1puppetlabs1'",
           :required     => "optional",
           :default      => "3.7.4-1puppetlabs1",
           :category     => "NEXTDOOR: PUPPET SETTINGS",
@@ -104,16 +115,21 @@ attribute "PUPPET_CA_SERVER",
 
 attribute "PUPPET_CHALLENGE_PASSWORD",
           :display_name => "PUPPET_CHALLENGE_PASSWORD",
-          :description  =>
-          "Puppet 3.4+ supports the ability to pass data to the Puppet Master through the CSR itself. If this option is supplied, the csr_attributes.yml file is created and the challengePassword option is embedded into the CSR.",
+          :description  => "Puppet 3.4+ supports the ability to pass data to"\
+                           " the Puppet Master through the CSR itself. If this"\
+                           " option is supplied, the csr_attributes.yml file is"\
+                           " created and the challengePassword option is"\
+                           " embedded into the CSR.",
           :required     => "recommended",
           :category     => "NEXTDOOR: PUPPET SETTINGS",
           :recipes      => [ "nextdoor::puppet-install" ]
 
 attribute "PUPPET_CUSTOM_FACTS",
           :display_name => "PUPPET_CUSTOM_FACTS",
-          :description  =>
-          "A list of key=value custom puppet facts that will be stored in /etc/facter/facts.d/nd-puppet.txt and available to Puppet as facts for your manifest compilation. eg: my_cname=foobar",
+          :description  => "A list of key=value custom puppet facts that will"\
+                           " be stored in /etc/facter/facts.d/nd-puppet.txt and"\
+                           " available to Puppet as facts for your manifest"\
+                           " compilation. eg: my_cname=foobar",
           :required     => "optional",
           :type         => "array",
           :default      => '',
@@ -122,8 +138,9 @@ attribute "PUPPET_CUSTOM_FACTS",
 
 attribute "PUPPET_ENABLE_REPORTS",
           :display_name => "PUPPET_ENABLE_REPORTS",
-          :description  =>
-          "Whether or not to send back Puppet Reports. Requires that your Puppet server is configured to accept reports and handle them.",
+          :description  => "Whether or not to send back Puppet Reports."\
+                           " Requires that your Puppet server is configured to"\
+                           " accept reports and handle them.",
           :default      => "true",
           :choice       => [ "true", "false" ],
           :required     => "optional",
@@ -140,14 +157,19 @@ attribute "PUPPET_ENVIRONMENT_NAME",
 
 attribute "PUPPET_NODE",
           :display_name => "PUPPET_NODE",
-          :description  => "If specified along with PUPPET_NODE_FACT='puppet_node', set the Puppet node name to this value.",
+          :description  => "If specified along with"\
+                           " PUPPET_NODE_FACT='puppet_node', set the Puppet"\
+                           " node name to this value.",
           :required     => "recommended",
           :category     => "NEXTDOOR: PUPPET SETTINGS",
           :recipes      => [ "nextdoor::puppet-install" ]
 
 attribute "PUPPET_NODE_NAME",
           :display_name => "PUPPET_NODE_NAME",
-          :description  => "Override Puppet node name specified in PUPPET_NODE via specified method: <cert|facter>. If 'facter', use the value returned by execution of PUPPET_NODE_FACT.",
+          :description  => "Override Puppet node name specified in"\
+                           " PUPPET_NODE via specified method: <cert|facter>."\
+                           " If 'facter', use the value returned by execution"\
+                           " of PUPPET_NODE_FACT.",
           :default      => "facter",
           :choice       => [ "facter", "cert" ],
           :required     => "optional",
@@ -156,7 +178,8 @@ attribute "PUPPET_NODE_NAME",
 
 attribute "PUPPET_NODE_NAME_FACT",
           :display_name => "PUPPET_NODE_NAME_FACT",
-          :description  => "PUPPET_NODE_NAME will be generated via the specified Facter fact.",
+          :description  => "PUPPET_NODE_NAME will be generated via the"\
+                           " specified Facter fact.",
           :required     => "optional",
           :default      => "hostname",
           :category     => "NEXTDOOR: PUPPET SETTINGS",
@@ -164,8 +187,7 @@ attribute "PUPPET_NODE_NAME_FACT",
 
 attribute "PUPPET_SERVER_HOSTNAME",
           :display_name => "PUPPET_SERVER_HOSTNAME",
-          :description  =>
-          "Puppet server to use for manifest compilation.",
+          :description  => "Puppet server to use for manifest compilation.",
           :default      => "puppet.nextdoor.com",
           :required     => "recommended",
           :category     => "NEXTDOOR: PUPPET SETTINGS",
@@ -221,7 +243,8 @@ attribute "STORAGE_RAID_LEVEL",
 attribute "STORAGE_SIZE",
           :category => "STORAGE",
           :display_name => "STORAGE_SIZE",
-          :description => "IF STORAGE_TYPE == EBS: This is the total size of the array to create. (in GB)",
+          :description => "IF STORAGE_TYPE == EBS: This is the total size of"\
+                          " the array to create. (in GB)",
           :required => "optional",
           :type => "string",
           :default => "512",
@@ -231,7 +254,11 @@ attribute "STORAGE_SIZE",
 attribute "STORAGE_TYPE",
           :category => "STORAGE",
           :display_name => "STORAGE_TYPE",
-          :description => "If storage type is 'instance', local normal storage is used. If type is 'ebs', a whole new set of EBS volumes will be created. If 'remount_ebs', we will re-mount EBS volumes supplied in the STORAGE_VOLIDLIST",
+          :description => "If storage type is 'instance', local normal storage"\
+                          " is used. If type is 'ebs', a whole new set of EBS"\
+                          " volumes will be created. If 'remount_ebs', we will"\
+                          " re-mount EBS volumes supplied in the"\
+                          " STORAGE_VOLIDLIST",
           :required => "optional",
           :type => "string",
           :default => "instance",
@@ -241,7 +268,8 @@ attribute "STORAGE_TYPE",
 attribute "STORAGE_VOLCOUNT",
           :category => "STORAGE",
           :display_name => "STORAGE_VOLCOUNT",
-          :description => "IF STORAGE_TYPE == EBS: The number of volumes to create within EC2 and join to the RAID array.",
+          :description => "IF STORAGE_TYPE == EBS: The number of volumes to"\
+                          " create within EC2 and join to the RAID array.",
           :required => "optional",
           :type => "string",
           :default => "0",
@@ -252,9 +280,9 @@ attribute "STORAGE_VOLIDLIST",
           :category => "STORAGE",
           :display_name => "STORAGE_VOLIDLIST",
           :description => "IF STORAGE_TYPE == remounte_ebs: Comma separated"\
-                          "list of EBS Volume IDS to try to mount on boot up"\
-                          "and re-join together as a RAID volume. This is used"\
-                          "for recovery only.",
+                          " list of EBS Volume IDS to try to mount on boot up"\
+                          " and re-join together as a RAID volume. This is used"\
+                          " for recovery only.",
           :required => "optional",
           :type => "array",
           :recipes => ['nextdoor::mounts']
