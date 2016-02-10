e31046bfc711dfe828248b83768627e1fceb7b5d
Nathan R Valentine <nrvale0@gmail.com>
2016-02-10 15:22:12 -0800

    sure would be nice to have a linter...

--- nextdoor/metadata.rb
+++ nextdoor/metadata.rb
@@ -1,7 +1,7 @@
 name        "nextdoor"
 maintainer  "Nextdoor.com"
 license     "see LICENSE file in repository root"
-description "Nextdoor.com RL10 (only!) RightScripts",
+description "Nextdoor.com RL10 (only!) RightScripts"
 version     '0.1.0'
 
 recipe      "nextdoor::dump-env", "Print environment variables for debugging"
