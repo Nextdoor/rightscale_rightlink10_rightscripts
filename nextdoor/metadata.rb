8d06d0df6bc84a6cbec57015d9328c17b5a7e0ca
Nathan R Valentine <nrvale0@gmail.com>
2016-02-10 14:27:47 -0800

    y,rly

--- nextdoor/metadata.rb
+++ nextdoor/metadata.rb
@@ -11,46 +11,6 @@ recipe      "nextdoor::puppet-install", "Install the Puppet agent"
 recipe      "nextdoor::elb-connect", "Register to an ELB"
 recipe      "nextdoor::elb-disconnect", "Deregister to an ELB"
 
-attribute "RS_CLOUD_PROVIDER",
-          :category => "CLOUD",
-          :display_name => "RS_CLOUD_PROVIDER",
-          :description => "In what cloud context are we running? <ec2|google>",
-          :required => "optional",
-          :type => "string",
-          :choice => ['ec2', 'google'],
-          :default => "ec2",
-          :recipes => ['nextdoor::mounts', 'nextdoor::hostname']
-
-attribute "STORAGE_RAID_LEVEL",
-          :category => "STORAGE",
-          :display_name => "STORAGE_RAID_LEVEL",
-          :description => "RAID level to use (0, 1, 5 or 10)",
-          :required => "optional",
-          :type => "string",
-          :default => "0",
-          :choice => ['0', '1', '5', '10'],
-          :recipes => ['nextdoor::mounts']
-
-attribute "STORAGE_FSTYPE",
-          :category => "STORAGE",
-          :display_name => "STORAGE_FSTYPE",
-          :description => "filesystem format for /mnt",
-          :required => "optional",
-          :type => "string",
-          :choice => ['ext3', 'ext4', 'xfs'],
-          :default => "xfs",
-          :recipes => ['nextdoor::mounts']
-
-attribute "STORAGE_VOLCOUNT",
-          :category => "STORAGE",
-          :display_name => "STORAGE_VOLCOUNT",
-          :description => "IF STORAGE_TYPE == EBS: The number of volumes to create within EC2 and join to the RAID array.",
-          :required => "optional",
-          :type => "string",
-          :default => "0",
-          :choice => ['0', '4', '6', '8'],
-          :recipes => ['nextdoor::mounts']
-
 attribute "AWS_ACCESS_KEY_ID",
           :category => "CLOUD",
           :display_name => "AWS_ACCESS_KEY_ID",
@@ -66,54 +26,6 @@ attribute "AWS_SECRET_ACCESS_KEY",
           :required => "required",
           :type => "string",
           :recipes => ['nextdoor::mounts', 'nextdoor::elb-connect', 'nextdoor::elb-disconnect']
-
-attribute "STORAGE_TYPE",
-          :category => "STORAGE",
-          :display_name => "STORAGE_TYPE",
-          :description => "If storage type is 'instance', local normal storage is used. If type is 'ebs', a whole new set of EBS volumes will be created. If 'remount_ebs', we will re-mount EBS volumes supplied in the STORAGE_VOLIDLIST",
-          :required => "optional",
-          :type => "string",
-          :default => "instance",
-          :choice => ['instance', 'ebs', 'remount-ebs'],
-          :recipes => ['nextdoor::mounts']
-
-attribute "EBS_TYPE",
-          :category => "STORAGE",
-          :display_name => "EBS_TYPE",
-          :description => "gp2 for General Purpose (SSD) volumes, io1 for Provisioned IOPS (SSD) volumes, or standard for Magnetic volumes. If you choose io1, you'll get 4000 IOPS.",
-          :required => "optional",
-          :type => "string",
-          :default => "standard",
-          :choice => ['standard', 'gp2', 'io1'],
-          :recipes => ['nextdoor::mounts']
-
-attribute "STORAGE_VOLIDLIST",
-          :category => "STORAGE",
-          :display_name => "STORAGE_VOLIDLIST",
-          :description => "IF STORAGE_TYPE == remounte_ebs: Comma separated list of EBS Volume IDS to try to mount on boot up and re-join together as a RAID volume. This is used for recovery only.
-",
-          :required => "optional",
-          :type => "array",
-          :recipes => ['nextdoor::mounts']
-
-attribute "STORAGE_SIZE",
-          :category => "STORAGE",
-          :display_name => "STORAGE_SIZE",
-          :description => "IF STORAGE_TYPE == EBS: This is the total size of the array to create. (in GB)",
-          :required => "optional",
-          :type => "string",
-          :default => "512",
-          :choice => ['512', '1024', '2048'],
-          :recipes => ['nextdoor::mounts']
-
-attribute "STORAGE_MOUNTPOINT",
-          :category => "STORAGE",
-          :display_name => "STORAGE_MOUNTPOINT",
-          :description => "Where is the mount volume created?",
-          :required => "optional",
-          :type => "string",
-          :default => "/mnt",
-          :recipes => ['nextdoor::mounts']
           
 attribute "DEBUG",
           :category => "NEXTDOOR",
@@ -132,6 +44,40 @@ attribute "DEFAULT_DOMAIN",
           :type => "string",
           :recipes => ['nextdoor::dump-env', 'nextdoor::hostname']
 
+attribute "EBS_TYPE",
+          :category => "STORAGE",
+          :display_name => "EBS_TYPE",
+          :description => "gp2 for General Purpose (SSD) volumes, io1 for Provisioned IOPS (SSD) volumes, or standard for Magnetic volumes. If you choose io1, you'll get 4000 IOPS.",
+          :required => "optional",
+          :type => "string",
+          :default => "standard",
+          :choice => ['standard', 'gp2', 'io1'],
+          :recipes => ['nextdoor::mounts']
+
+attribute "EC2_PLACEMENT_AVAILABILITY_ZONE",
+          :category => "LOAD_BALANCER",
+          :display_name => "EC2_PLACEMENT_AVAILABILITY_ZONE",
+          :description => "??",
+          :required => "optional",
+          :style => "string",
+          :recipes => ['nextdoor::elb-connect', 'nextdoor::elb-disconnect']
+
+attribute "EC2_INSTANCE_ID",
+          :category => "LOAD_BALANCER",
+          :display_name => "EC2_INSTANCE_ID",
+          :description => "AWS EC2 instance ID for LB reg/de-reg",
+          :require => "optional",
+          :style => "string",
+          :recipes => ['nextdoor::elb-connect', 'nextdoor::elb-disconnect']
+
+attribute "ELB_NAME",
+          :category => "LOAD_BALANCER",
+          :display_name => "ELB_NAME",
+          :description => "The is the name of the ELB you gave in RightScale or the Amazon AWS console. Note: This is the short name and *not* the FQDN.",
+          :required => "optional",
+          :type => "string",
+          :recipes => ['nextdoor::elb-connect', 'nextdoor::elb-disconnect']
+
 attribute "INSTANCE_ID",
           :category => "NEXTDOOR: HOSTNAME SETTINGS",
           :display_name => "INSTANCE_ID",
@@ -139,14 +85,6 @@ attribute "INSTANCE_ID",
           :require => "optional",
           :type => "string",
           :recipes => ['nextdoor::dump-env', 'nextdoor::hostname', 'nextdoor::elb-connect', 'nextdoor::elb-disconnect']
-
-attribute "SERVER_NAME",
-          :category => "NEXTDOOR: HOSTNAME SETTINGS",
-          :display_name => "SERVER_NAME",
-          :description => "Human Readable Server Name (ie 'my web server 1')",
-          :require => "optional",
-          :type => "string",
-          :recipes => ['nextdoor:dump-env', 'nextdoor::hostname']
 
 attribute "PUPPET_AGENT_VERSION",
           :display_name => "PUPPET_AGENT_VERSION",
@@ -156,6 +94,22 @@ attribute "PUPPET_AGENT_VERSION",
           :category     => "NEXTDOOR: PUPPET SETTINGS",
           :recipes      => [ "nextdoor::puppet-install"]
 
+attribute "PUPPET_CA_SERVER",
+          :display_name => "PUPPET_CA_SERVER",
+          :description  => "Puppet server to use for certificate requests.",
+          :required     => "recommended",
+          :default      => "puppetca.nextdoor.com",
+          :category     => "NEXTDOOR: PUPPET SETTINGS",
+          :recipes      => [ "nextdoor::puppet-install" ]
+
+attribute "PUPPET_CHALLENGE_PASSWORD",
+          :display_name => "PUPPET_CHALLENGE_PASSWORD",
+          :description  =>
+          "Puppet 3.4+ supports the ability to pass data to the Puppet Master through the CSR itself. If this option is supplied, the csr_attributes.yml file is created and the challengePassword option is embedded into the CSR.",
+          :required     => "recommended",
+          :category     => "NEXTDOOR: PUPPET SETTINGS",
+          :recipes      => [ "nextdoor::puppet-install" ]
+
 attribute "PUPPET_CUSTOM_FACTS",
           :display_name => "PUPPET_CUSTOM_FACTS",
           :description  =>
@@ -163,6 +117,24 @@ attribute "PUPPET_CUSTOM_FACTS",
           :required     => "optional",
           :type         => "array",
           :default      => '',
+          :category     => "NEXTDOOR: PUPPET SETTINGS",
+          :recipes      => [ "nextdoor::puppet-install" ]
+
+attribute "PUPPET_ENABLE_REPORTS",
+          :display_name => "PUPPET_ENABLE_REPORTS",
+          :description  =>
+          "Whether or not to send back Puppet Reports. Requires that your Puppet server is configured to accept reports and handle them.",
+          :default      => "true",
+          :choice       => [ "true", "false" ],
+          :required     => "optional",
+          :category     => "NEXTDOOR: PUPPET SETTINGS",
+          :recipes      => [ "nextdoor::puppet-install" ]
+
+attribute "PUPPET_ENVIRONMENT_NAME",
+          :display_name => "PUPPET_ENVIRONMENT_NAME",
+          :description  => "Puppet environment to request",
+          :default      => "production",
+          :required     => "recommended",
           :category     => "NEXTDOOR: PUPPET SETTINGS",
           :recipes      => [ "nextdoor::puppet-install" ]
 
@@ -182,37 +154,11 @@ attribute "PUPPET_NODE_NAME",
           :category     => "NEXTDOOR: PUPPET SETTINGS",
           :recipes      => [ "nextdoor::puppet-install" ]
 
-attribute "PUPPET_ENABLE_REPORTS",
-          :display_name => "PUPPET_ENABLE_REPORTS",
-          :description  =>
-          "Whether or not to send back Puppet Reports. Requires that your Puppet server is configured to accept reports and handle them.",
-          :default      => "true",
-          :choice       => [ "true", "false" ],
-          :required     => "optional",
-          :category     => "NEXTDOOR: PUPPET SETTINGS",
-          :recipes      => [ "nextdoor::puppet-install" ]
-
 attribute "PUPPET_NODE_NAME_FACT",
           :display_name => "PUPPET_NODE_NAME_FACT",
           :description  => "PUPPET_NODE_NAME will be generated via the specified Facter fact.",
           :required     => "optional",
           :default      => "hostname",
-          :category     => "NEXTDOOR: PUPPET SETTINGS",
-          :recipes      => [ "nextdoor::puppet-install" ]
-
-attribute "PUPPET_CHALLENGE_PASSWORD",
-          :display_name => "PUPPET_CHALLENGE_PASSWORD",
-          :description  =>
-          "Puppet 3.4+ supports the ability to pass data to the Puppet Master through the CSR itself. If this option is supplied, the csr_attributes.yml file is created and the challengePassword option is embedded into the CSR.",
-          :required     => "recommended",
-          :category     => "NEXTDOOR: PUPPET SETTINGS",
-          :recipes      => [ "nextdoor::puppet-install" ]
-
-attribute "PUPPET_CA_SERVER",
-          :display_name => "PUPPET_CA_SERVER",
-          :description  => "Puppet server to use for certificate requests.",
-          :required     => "recommended",
-          :default      => "puppetca.nextdoor.com",
           :category     => "NEXTDOOR: PUPPET SETTINGS",
           :recipes      => [ "nextdoor::puppet-install" ]
 
@@ -225,37 +171,90 @@ attribute "PUPPET_SERVER_HOSTNAME",
           :category     => "NEXTDOOR: PUPPET SETTINGS",
           :recipes      => [ "nextdoor::puppet-install" ]
 
-attribute "PUPPET_ENVIRONMENT_NAME",
-          :display_name => "PUPPET_ENVIRONMENT_NAME",
-          :description  => "Puppet environment to request",
-          :default      => "production",
-          :required     => "recommended",
-          :category     => "NEXTDOOR: PUPPET SETTINGS",
-          :recipes      => [ "nextdoor::puppet-install" ]
-
-attribute "ELB_NAME",
-          :category => "LOAD_BALANCER",
-          :display_name => "ELB_NAME",
-          :description => "The is the name of the ELB you gave in RightScale or the Amazon AWS console. Note: This is the short name and *not* the FQDN.",
+attribute "RS_CLOUD_PROVIDER",
+          :category => "CLOUD",
+          :display_name => "RS_CLOUD_PROVIDER",
+          :description => "In what cloud context are we running? <ec2|google>",
           :required => "optional",
           :type => "string",
-          :recipes => ['nextdoor::elb-connect', 'nextdoor::elb-disconnect']
+          :choice => ['ec2', 'google'],
+          :default => "ec2",
+          :recipes => ['nextdoor::mounts', 'nextdoor::hostname']
 
-attribute "EC2_PLACEMENT_AVAILABILITY_ZONE",
-          :category => "LOAD_BALANCER",
-          :display_name => "EC2_PLACEMENT_AVAILABILITY_ZONE",
-          :description => "??",
-          :required => "optional",
-          :style => "string",
-          :recipes => ['nextdoor::elb-connect', 'nextdoor::elb-disconnect']
-
-attribute "EC2_INSTANCE_ID",
-          :category => "LOAD_BALANCER",
-          :display_name => "EC2_INSTANCE_ID",
-          :description => "AWS EC2 instance ID for LB reg/de-reg",
+attribute "SERVER_NAME",
+          :category => "NEXTDOOR: HOSTNAME SETTINGS",
+          :display_name => "SERVER_NAME",
+          :description => "Human Readable Server Name (ie 'my web server 1')",
           :require => "optional",
-          :style => "string",
-          :recipes => ['nextdoor::elb-connect', 'nextdoor::elb-disconnect']
+          :type => "string",
+          :recipes => ['nextdoor:dump-env', 'nextdoor::hostname']
 
+attribute "STORAGE_FSTYPE",
+          :category => "STORAGE",
+          :display_name => "STORAGE_FSTYPE",
+          :description => "filesystem format for /mnt",
+          :required => "optional",
+          :type => "string",
+          :choice => ['ext3', 'ext4', 'xfs'],
+          :default => "xfs",
+          :recipes => ['nextdoor::mounts']
 
+attribute "STORAGE_MOUNTPOINT",
+          :category => "STORAGE",
+          :display_name => "STORAGE_MOUNTPOINT",
+          :description => "Where is the mount volume created?",
+          :required => "optional",
+          :type => "string",
+          :default => "/mnt",
+          :recipes => ['nextdoor::mounts']
 
+attribute "STORAGE_RAID_LEVEL",
+          :category => "STORAGE",
+          :display_name => "STORAGE_RAID_LEVEL",
+          :description => "RAID level to use (0, 1, 5 or 10)",
+          :required => "optional",
+          :type => "string",
+          :default => "0",
+          :choice => ['0', '1', '5', '10'],
+          :recipes => ['nextdoor::mounts']
+
+attribute "STORAGE_SIZE",
+          :category => "STORAGE",
+          :display_name => "STORAGE_SIZE",
+          :description => "IF STORAGE_TYPE == EBS: This is the total size of the array to create. (in GB)",
+          :required => "optional",
+          :type => "string",
+          :default => "512",
+          :choice => ['512', '1024', '2048'],
+          :recipes => ['nextdoor::mounts']
+
+attribute "STORAGE_TYPE",
+          :category => "STORAGE",
+          :display_name => "STORAGE_TYPE",
+          :description => "If storage type is 'instance', local normal storage is used. If type is 'ebs', a whole new set of EBS volumes will be created. If 'remount_ebs', we will re-mount EBS volumes supplied in the STORAGE_VOLIDLIST",
+          :required => "optional",
+          :type => "string",
+          :default => "instance",
+          :choice => ['instance', 'ebs', 'remount-ebs'],
+          :recipes => ['nextdoor::mounts']
+
+attribute "STORAGE_VOLCOUNT",
+          :category => "STORAGE",
+          :display_name => "STORAGE_VOLCOUNT",
+          :description => "IF STORAGE_TYPE == EBS: The number of volumes to create within EC2 and join to the RAID array.",
+          :required => "optional",
+          :type => "string",
+          :default => "0",
+          :choice => ['0', '4', '6', '8'],
+          :recipes => ['nextdoor::mounts']
+
+attribute "STORAGE_VOLIDLIST",
+          :category => "STORAGE",
+          :display_name => "STORAGE_VOLIDLIST",
+          :description => "IF STORAGE_TYPE == remounte_ebs: Comma separated"\
+                          "list of EBS Volume IDS to try to mount on boot up"\
+                          "and re-join together as a RAID volume. This is used"\
+                          "for recovery only.",
+          :required => "optional",
+          :type => "array",
+          :recipes => ['nextdoor::mounts']
